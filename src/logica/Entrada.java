63a86071fa4dfc253d28bfbb2a7919e75e81c189
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-06-08 00:33:46 +0200

    ya
    ya

--- src/logica/Entrada.java
+++ src/logica/Entrada.java
@@ -1,5 +1,57 @@
 package logica;
 
-public class entrada {
+//import java.lang.Float;
+import logica.Operacion;
 
+public class Entrada {
+
+	private String display;
+	
+	public Entrada(String display) {
+		super();
+		this.display = display;
+	}
+
+
+	public int validacion(){
+		boolean parada=true;
+		int i=1;
+		
+		while(i<display.length() && parada){
+			if(!Character.isDigit(display.charAt(i)) && display.charAt(i)!='.'){
+				parada=false;
+			}	
+		}
+		if(display.charAt(i)=='/' && display.substring(i+1).equals("0"));
+			i=-1;
+	return i;
+	// puedes invocar este metodo dentro del metodo de resultado xq si es -1 quiere decir que hay division x cero
+	}
+	
+	public Operacion resultado(int i){
+		
+		double num1=0;
+		double num2=0;
+		char operador;
+		
+		if(i!=display.length()){
+			
+			String snum1=display.substring(0, i-1);
+			num1=Double.parseDouble(snum1);
+			
+			String snum2=display.substring(i, i-1);
+			num2=Double.parseDouble(snum2);
+			
+			operador=display.charAt(i);
+			
+		}else{
+			num1=Double.parseDouble(display);
+			operador='+';
+		}
+		
+		Operacion operacion = new Operacion(num1, num2, operador);
+		
+		return operacion;
+	}
+	
 }
--- src/logica/operacion.java
+++ src/logica/operacion.java
@@ -2,35 +2,35 @@ package logica;
 
 public class Operacion {
 	
-	private float num1;
-	private float num2;
+	private double num1;
+	private double num2;
 	private char operador;
 	
-	public Operacion(float num1, float num2, char operador) {
+	public Operacion(double num1, double num2, char operador) {
 		super();
 		this.num1 = num1;
 		this.num2 = num2;
 		this.operador = operador;
 	}
 	
-	private float suma(){
+	private double suma(){
 		return num1+num2;
 	}
 	
-	private float resta(){
+	private double resta(){
 		return num1-num2;
 	}
 	
-	private float multiplicacion(){
+	private double multiplicacion(){
 		return num1*num2;
 	}
 	
-	private float division(){
+	private double division(){
 		return num1/num2;
 	}
 	
-	public float controlador(){
-		float result=0;
+	public double controlador(){
+		double result=0;
 		if(operador=='+')
 			result=suma();
 		else if(operador=='-')
