894d33d370b377f33c689924c73c3779e82885e9
Rafael García González <rgarciagonzalez59@gmail.com>
2024-06-11 03:38:19 -0400

    Delete src directory

--- src/gui/utils/Definiciones.java
+++
@@ -1,9 +0,0 @@
-package gui.utils;
-
-public class Definiciones {
-
-	public static int cantPuntoFlotante = 0;
-	public static boolean operador = false;
-	public static boolean nuevoNum = false;
-	
-}
